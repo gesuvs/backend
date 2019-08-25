d5861be24b279e14e2b03004d7a39a9116306b31
GBernardo10 <g_bernardo10@hotmail.com>
2019-08-24 22:40:11 -0300

    modifica arquivo pom.xml, configuracao da api de sms

---
+++ src/main/java/br/com/bandtec/bora/controller/api/sms/Administrador.java
@@ -0,0 +1,22 @@
+package br.com.bandtec.bora.controller.api.sms;
+
+public class Administrador {
+	private String nome;
+	private String celular;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	
+	
+	
+}
