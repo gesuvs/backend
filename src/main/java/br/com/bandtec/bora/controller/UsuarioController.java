2c6e672ab007ef8ab71a2c52e720d6a58ced884f
GBernardo10 <g_bernardo10@hotmail.com>
2019-08-29 23:42:39 -0300

    adiciona as configurações para ser seguida nas outras branch

--- src/main/java/br/com/bandtec/bora/controller/UsuarioController.java
+++ src/main/java/br/com/bandtec/bora/controller/UsuarioController.java
@@ -42,24 +42,3 @@ public class UsuarioController {
 	}
 
 }
-//@RestController
-//public class UsuarioController {
-//	@Autowired
-//	private UsuarioRepository usuarioRepository;
-//	private List<Usuario> obterTodosUsuarios;
-//
-//	public UsuarioController() {
-//		this.obterTodosUsuarios = todosUsuarios();
-//	}
-//
-//	@GetMapping("/usuarios")
-//	public ResponseEntity<List<Usuario>> usuariosTodos(Usuario usuario){
-//		return ResponseEntity.ok(usuarioRepository.findAll());
-//	}
-//
-//	private List<Usuario> todosUsuarios() {
-//		return obterTodosUsuarios;
-//
-//	}
-
-//}
