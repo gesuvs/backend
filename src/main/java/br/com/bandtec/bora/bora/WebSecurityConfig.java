1c4a91cd552dfc0b8fca34706587c83dba5f10b6
bruno.arruda <bruno.arruda@tivit.com>
2019-09-08 16:28:57 -0300

    login em memoria

--- src/main/java/br/com/bandtec/bora/bora/WebSecurityConfig.java
+++ src/main/java/br/com/bandtec/bora/bora/WebSecurityConfig.java
@@ -4,48 +4,46 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
+    
+	@Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/home", "/api**", "/api/usuarios**", "/api**")
-                	.permitAll()
-            .anyRequest()
-            	.authenticated().and()
-            .formLogin()
-            	.loginPage("/login")
-                	.permitAll().and()
-            .logout()
-                .permitAll();
+		http.csrf().disable().authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/").permitAll()
+		.antMatchers(HttpMethod.GET, "/api/usuarios").permitAll()
+		.antMatchers(HttpMethod.POST, "/api/cadastrar-usuario").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin().permitAll()
+		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+//		http.csrf().disable()
+//            .authorizeRequests()
+//                .antMatchers("/", "/home", "/api**", "/api/usuarios**")
+//                	.permitAll()
+//            .anyRequest()
+//            	.authenticated().and()
+//            .formLogin()
+//            	.loginPage("/login")
+//                	.permitAll().and()
+//            .logout()
+//                .permitAll();
     }
 	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//			.authorizeRequests()
-//				.antMatchers(HttpMethod.POST, "/api/cadastrar-usuario").permitAll()
-//				.antMatchers(HttpMethod.GET, "/", "home", "/api/usuarios").permitAll()
-//				.anyRequest().authenticated()
-//				.and()
-//			.formLogin()
-//				.loginPage("/login")
-//				.permitAll()
-//				.and()
-//			.logout()
-//				.permitAll();
-//			
-//	}
-
+	@Override
+	public void configure(WebSecurity webSecurity) throws Exception {
+	    webSecurity.ignoring().antMatchers(HttpMethod.POST, "/api/cadastro-usuario");
+	}
+	
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
