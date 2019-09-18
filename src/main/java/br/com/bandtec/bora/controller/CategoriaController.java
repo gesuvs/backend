ee928ce0d5a902ac6c9ba29f344410d4c63a3ea4
BielzerA23 <gabriel.ribeiro20110@hotmail.com>
2019-09-18 18:04:50 -0300

    Conexao h2 e json atualizado

--- src/main/java/br/com/bandtec/bora/controller/CategoriaController.java
+++ src/main/java/br/com/bandtec/bora/controller/CategoriaController.java
@@ -4,11 +4,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.bandtec.bora.model.entity.Categoria;
 import br.com.bandtec.bora.model.service.CategoriaService;
 
 @RestController
+@RequestMapping("/api")
 public class CategoriaController {
 
 	@Autowired
