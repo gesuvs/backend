ef362cb21213d5279e4adf99b0ec3ee8d3a56c88
bruno.arruda <bruno.arruda@tivit.com>
2019-10-11 17:21:43 -0300

    Sub Categoria

--- src/main/java/br/com/bandtec/bora/model/entity/SubCategoria.java
+++ src/main/java/br/com/bandtec/bora/model/entity/SubCategoria.java
@@ -1,11 +1,11 @@
 package br.com.bandtec.bora.model.entity;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
@@ -15,16 +15,50 @@ import javax.validation.constraints.NotEmpty;
 public class SubCategoria {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_sub_categoria")
 	private Long id;
 	
 	@Column(name = "nome_sub_categoria")
 	private String nome;
 	
-//	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
-//	@Column(name="id_categoria")
-//	@NotEmpty
-//	private Categoria idCategoria;
+	@ManyToOne
+	@JoinColumn(name = "idCategoria")
+	@NotEmpty
+	private Categoria categoria;
+	
+	
 
+	public SubCategoria() {
+	}
+
+	public SubCategoria(Long id, String nome, @NotEmpty Categoria categoria) {
+		this.id = id;
+		this.nome = nome;
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 }
